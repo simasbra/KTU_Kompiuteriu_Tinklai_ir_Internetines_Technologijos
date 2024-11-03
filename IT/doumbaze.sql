eb6ca3cd14eac28cc5c3285ddc2c24f91b281ca5
Simas Bradaitis <simasbra@proton.me>
2024-11-03 22:41:54 +0200

    sql update

--- IT/doumbaze.sql
+++ IT/doumbaze.sql
@@ -1,12 +1,15 @@
+create database IT;
+use IT;
+
 create table Paskyros_tipas (
-	id int not null,
+	id int not null auto_increment,
 	name varchar(255) not null,
 
 	primary key (id)
 );
 
 create table Vartotojas (
-	id int not null,
+	id int not null auto_increment,
 	prisijungimo_vardas varchar(255) not null,
 	slaptazodis varchar(255) not null,
 	vardas varchar(255) not null,
@@ -14,11 +17,11 @@ create table Vartotojas (
 	paskyros_tipas_id int not null,
 
 	primary key (id),
-	foreign key (paskyros_tipas_id) references Pasyros_tipas(id)
+	foreign key (paskyros_tipas_id) references Paskyros_tipas(id)
 );
 
 create table Straipsnis (
-	id int not null,
+	id int not null auto_increment,
 	pavadinimas varchar(255) not null,
 	tema varchar(255) not null,
 	tekstas text not null,
@@ -30,11 +33,12 @@ create table Straipsnis (
 );
 
 create table Vertinimas (
-	id int not null,
+	id int not null auto_increment,
 	vertinimas int not null,
 	vartotojas_id int not null,
 
 	primary key (id),
 	foreign key (vartotojas_id) references Vartotojas(id)
-
 );
+
+insert into Paskyros_tipas (name) values ("Vartotojas"), ("Vadybininkas");
