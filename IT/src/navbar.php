cddbfa3bb2d855daf2e3c28700ea1b3292e9691f
Simas Bradaitis <simasbra@proton.me>
2024-11-10 20:30:00 +0200

    IT navbar update

--- IT/src/navbar.php
+++ IT/src/navbar.php
@@ -7,9 +7,14 @@
         <li><a href="">Statistika</a></li>
 
         <?php if (isset($_SESSION['user_id'])): ?>
-            <!-- User is logged in -->
-            <li><a href="logout.php">Atsijungti</a></li>
-            <li><a href=""><?php echo htmlspecialchars($_SESSION['username']); ?> <?php echo htmlspecialchars($_SESSION['user_name']); ?></a></li>
+            <!-- User profile with dropdown -->
+            <li class="dropdown">
+                <a href="#" class="dropbtn"><?php echo htmlspecialchars($_SESSION['user_name']); ?></a>
+                <div class="dropdown-content">
+                    <a href="profile.php">Profilis</a>
+                    <a href="logout.php">Atsijungti</a>
+                </div>
+            </li>
         <?php else: ?>
             <!-- User is not logged in -->
             <li><a href="login.php">Prisijungti</a></li>
