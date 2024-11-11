c4285948a3b5b94c72d2706d99942e3e87dfe806
Simas Bradaitis <simasbra@proton.me>
2024-11-11 16:55:27 +0200

    IT sql

--- IT/duombaze.sql
+++ IT/duombaze.sql
@@ -4,7 +4,7 @@ use IT;
 
 create table Paskyros_tipas (
 	id int not null auto_increment,
-	name varchar(255) not null,
+	pavadinimas varchar(255) not null,
 
 	primary key (id)
 );
@@ -21,21 +21,49 @@ create table Vartotojas (
 	foreign key (paskyros_tipas_id) references Paskyros_tipas(id)
 );
 
+create table Tema (
+	id int not null auto_increment,
+	pavadinimas varchar(255) not null,
+
+	primary key (id)
+);
+
 create table Straipsnis (
 	id int not null auto_increment,
 	pavadinimas varchar(255) not null,
-	tema varchar(255) not null,
-	tekstas text not null,
 	sukurimo_data datetime not null,
 	vartotojas_id int not null,
+	tema_id int not null,
 
 	primary key (id),
-	foreign key (vartotojas_id) references Vartotojas(id)
+	foreign key (vartotojas_id) references Vartotojas(id),
+	foreign key (tema_id) references Tema(id)
+);
+
+create table Paveikslelis (
+	id int not null auto_increment,
+	pavadinimas varchar(255) not null,
+	pozicija enum('kairėje', 'dešinėje', 'apačioje', 'viršuje') default 'apačioje' not null,
+	url varchar(1024) not null,
+
+	primary key (id)
+);
+
+create table Straipsnis_Blokas (
+	id int not null auto_increment,
+	tekstas text,
+	straipsnis_id int not null,
+	paveikslelis_id int,
+
+	primary key (id),
+	foreign key (straipsnis_id) references Straipsnis(id),
+	foreign key (paveikslelis_id) references Paveikslelis(id)
 );
 
 create table Vertinimas (
 	id int not null auto_increment,
 	vertinimas int not null,
+	sukurimo_data datetime not null,
 	vartotojas_id int not null,
 	straipsnis_id int not null,
 
@@ -44,4 +72,7 @@ create table Vertinimas (
 	foreign key (straipsnis_id) references Straipsnis(id)
 );
 
-insert into Paskyros_tipas (name) values ("Vartotojas"), ("Vadybininkas");
+insert into Paskyros_tipas (pavadinimas) values
+	("Skaitytojas"),
+	("Rašytojas"),
+	("Administratorius");
